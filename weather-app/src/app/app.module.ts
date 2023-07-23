97fae230e9b2dfd602f35ca78eaca7c4304137de
unknown <hobba_25@hotmail.com>
2023-07-23 21:25:02 +0100

    basic setup for weather component, default redirect in routing added

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { WeatherPageComponent } from './pages/weather-page/weather-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherPageComponent
   ],
   imports: [
     BrowserModule,
